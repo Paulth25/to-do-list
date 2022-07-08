b826c3ce44572618cc8e6e231f2722ffe68ee680
Paulth25 <paulthughes024@gmail.com>
2022-07-08 11:21:57 +0100

    to do list first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [newList, setnewList] = useState("");
+  let [items, setItems] = useState([]);
+  let [showEdit, setShowEdit] = useState(-1);
+  let [updatedText, setNewText] = useState("");
+
+  // Adding a new item into the array 
+  function addToList() {
+    // Checking for an empty item
+    if (!newList)
+    {
+      alert("You need to type something in the box before you can add it to the list sausage !");
+      return;
+    }
+
+    let item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newList,
+    };
+      // sorts the item ID with a new id generated.
+
+    // Adding new item to items list.
+    setItems((oldList) => [...oldList, item]);
+
+    // Reset newList back to original state
+    setnewList("");
+  }
+
+  // deletes an item
+  function deleteFromList(id) {
+    let newArray = items.filter((item) => item.id !== id);
+    setItems(newArray);
+  }
+
+  /* Edit an item text after creating it. */
+  function editItem(id, newText) {
+    // Get the current item
+    let currentItem = items.filter((item) => item.id === id);
+
+    // Create a new item with same id
+    let newList = {
+      id: currentItem.id,
+      value: newText,
+    };
+
+    deleteFromList(id);
+
+    // Replace item in the item list
+    setItems((oldList) => [...oldList, newList]);
+    setNewText("");
+    setShowEdit(-1);
+  }
+
+  // Main part of app
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+    <h1>Welcome to your</h1>
+    <h1>To-Do List</h1>
+    {/* 2. Add new item */}
+    <input
+        type="text"
+        placeholder="Add something to your list..."
+        value={newList}
+        onChange={(e) => setnewList(e.target.value)}
+    />
+  {/* button to add an item to the list */}
+    <button onClick={() => addToList()}>Add</button>
+  {/* 3. List of todos (unordered list) */}
+    <ul>
+        {items.map((item) => {
+        return (
+    <div>
+    <li key={item.id} onClick={() => setShowEdit(item.id)}>
+    {item.value}
+    <button
+        className="delete-button"
+        onClick={() => deleteFromList(item.id)}
         >
-          Learn React
-        </a>
-      </header>
+        ❌ 
+    </button>
+    </li>
+    
+    {showEdit == item.id ? (
+    <div>
+    <input
+        type="text"
+        value={updatedText}
+        onChange={(e) => setNewText(e.target.value)}
+    />
+    <button onClick={() => editItem(item.id, updatedText)}> Update
+    </button>
     </div>
-  );
-}
+    ) : null}
+    </div>
+    );})}
+    </ul></div>
+    );}
 
 export default App;
+
+// added some notes for personal reference
